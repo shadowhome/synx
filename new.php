0b3852d1f549a1452f71504c823e31f98624a3b8
System Administrator <root@Shadow-MacBook-Pro.local>
2015-08-20 12:49:10 +0100

    added update cron to add ability to update the remote db

--- new.php
+++ new.php
@@ -94,7 +94,7 @@ if ($_REQUEST['populate'] == 'yes') {
 	sshiconn($cmd, $pass, $ip, $sshp);
 	flush();
 	echo "Setting cronjobs and sudo access to perform upgrades when asked to";
-	$cmd="su - sysad -c 'mkdir -p /home/sysad/.ssh; chmod 700 /home/sysad/.ssh; echo \"$sshpub\" >> /home/sysad/.ssh/authorized_keys';echo \"10 1 * * * root /home/sysad/manage/packs.sh all\" >> /etc/crontab;echo \"sysad   ALL=(root)      NOPASSWD: /usr/bin/apt-get\" >> /etc/sudoers ";
+	$cmd="su - sysad -c 'mkdir -p /home/sysad/.ssh; chmod 700 /home/sysad/.ssh; echo \"$sshpub\" >> /home/sysad/.ssh/authorized_keys';echo \"10 1 * * * root /home/sysad/manage/packs.sh all\" >> /etc/crontab;echo \"sysad   ALL=(root)      NOPASSWD: /usr/bin/apt-get, /home/sysad/manage/packs.sh \" >> /etc/sudoers ";
 	sshiconn($cmd, $pass, $ip, $sshp);
 	flush();
 	
